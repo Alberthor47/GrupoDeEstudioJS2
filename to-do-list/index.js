5b1fc01bf1d44746ae867c4fa5486c227d4ba5e5
Alberto Camarena <me@acamrenar.com>
2021-05-06 22:01:37 -0500

    [ADD] 06/05 session

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,19 +1,35 @@
+// NO TIENEN HOISTING
+// let / const 
+// y arrow funtions tampoco
+// var miFuncionFlecha = () => {};
+
+// SI TIENE HOISTING
+// las funciones function y var
+
+
 const miBoton = document.getElementById('boton')
-miBoton.addEventListener('click', avisarCuandoSeHagaClick)
+miBoton.addEventListener('click', agregarTarea)
 
 const miEntrada = document.getElementById('entrada')
 
 const miLista = document.getElementById('lista')
 console.log(miLista)
 
+// let stringDeLista = ''; Esto es parte de la otra forma de agregar los items a la lista
+let numeroDeTarea = 0;
+
+// const crearNumeroAleatorio = () => ( Math.ceil(((Math.random()*1000000))) );
+
 let contenidoDeLista = [];
 let listaEnString;
 contenidoDeLista.push(localStorage.getItem('ListaDeTareas'));
 pintarListaEnHTML();
 
-// Esto es un comentario del hoisting
-function avisarCuandoSeHagaClick() {
-  let item = `<li> ${miEntrada.value} </li>\n`;
+function agregarTarea() {
+  //aqui agregaremos una tarea a la lista
+  // stringDeLista += `<li> ${miEntrada.value} </li>\n` //Metodo más simple para concatenar los items de la lista
+
+  let item = `<li> ${miEntrada.value}  <button id="${numeroDeTarea += 1}">Completado</button> </li>\n`;
   contenidoDeLista.push(item);
 
   pintarListaEnHTML();
@@ -23,4 +39,8 @@ function avisarCuandoSeHagaClick() {
 function pintarListaEnHTML () {
   listaEnString = contenidoDeLista.join('')
   miLista.innerHTML = listaEnString;
+}
+
+function elimarTarea() {
+  //aqui elimnaermos una tarea de la lista
 }
