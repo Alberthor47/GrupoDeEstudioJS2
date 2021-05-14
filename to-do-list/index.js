08a3618c35b4a8f14860c49f923194974442d522
Alberto Camarena <me@acamrenar.com>
2021-05-13 21:59:06 -0500

    [ADD] 13/05 session

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -6,24 +6,43 @@
 // SI TIENE HOISTING
 // las funciones function y var
 
-
 const miBoton = document.getElementById('boton')
 miBoton.addEventListener('click', agregarTarea)
-
 const miEntrada = document.getElementById('entrada')
-
 const miLista = document.getElementById('lista')
-console.log(miLista)
-
-// let stringDeLista = ''; Esto es parte de la otra forma de agregar los items a la lista
-let numeroDeTarea = 0;
-
-// const crearNumeroAleatorio = () => ( Math.ceil(((Math.random()*1000000))) );
 
 let contenidoDeLista = [];
 let listaEnString;
 contenidoDeLista.push(localStorage.getItem('ListaDeTareas'));
 pintarListaEnHTML();
+
+const i = function init() {
+  let lista = localStorage.getItem('ListaDeTareas');
+  console.log(lista)
+  if (lista) {
+    contenidoDeLista = lista.split(/\/li>/);
+    contenidoDeLista.forEach((item, index, arr) => arr[index] = item + '/li>');
+  }
+  console.log('Nuestra lista traida del ls', contenidoDeLista);
+}();
+
+// function HighOrderFunction(callBackFunction) {
+//   callBackFunction();
+// }
+
+// High order funtion
+// function saludar(funcToWrap, horaDelSaludo) {
+//   funcToWrap(horaDelSaludo);
+// }
+
+// funcion regular
+// function tipoDeSaludo(tiempo) {
+//   let saludo = 'Hola mi nombre es alberto y son las: ';
+//   console.log(saludo + tiempo);
+// }
+
+// En ejecucion
+// saludar(tipoDeSaludo, '12:30');
 
 function agregarTarea() {
   //aqui agregaremos una tarea a la lista
