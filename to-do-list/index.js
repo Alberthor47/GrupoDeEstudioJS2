db419e01d9dfd3e8e5967f83104d707203fd8dd5
Alberto Camarena <me@acamrenar.com>
2021-06-10 22:10:04 -0500

    [ADD] final project

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,89 +1,54 @@
-// NO TIENEN HOISTING
-// let / const 
-// y arrow funtions tampoco
-// var miFuncionFlecha = () => {};
-
-// SI TIENE HOISTING
-// las funciones function y var
-
 const miBoton = document.getElementById('boton')
 miBoton.addEventListener('click', agregarTarea)
 const miEntrada = document.getElementById('entrada')
 const miLista = document.getElementById('lista')
 
 let contenidoDeLista = [];
-let numeroDeReferencia = 0;
-
-let listaEnString;
-contenidoDeLista.push(localStorage.getItem('ListaDeTareas'));
-pintarListaEnHTML();
 
 const main = function init() {
-  let lista = localStorage.getItem('ListaDeTareas');
-  if (lista) {
-    contenidoDeLista = lista.split(/\/li>/);
-    contenidoDeLista.forEach((item, index, arr) => {
-        arr[index] = item + '/li>'
-    });
-  }
-
+  obtenerLocalStorage();
 }();
 
-function agregarTarea() {
-  let item = `<li> ${miEntrada.value} <button id=${numeroDeReferencia+1}>Completado</button> </li>\n`;
-  numeroDeReferencia ++;
-  // let botonDeItem = document.getElementById(`${numeroDeReferencia}`);
-  // botonDeItem.addEventListener('click', elimarTarea);
-  contenidoDeLista.push(item);
-
+function obtenerLocalStorage() {
+  let listaSinTrata = localStorage.getItem('ListaDeTareas');
+  contenidoDeLista = JSON.parse(listaSinTrata);
   pintarListaEnHTML();
-  localStorage.setItem('ListaDeTareas', listaEnString);
-
-  let longitudDeLaLista = miLista.children.length;
-
-  for (let i = 0; i < longitudDeLaLista; i++) {
-    let ele = miLista.children.item(i)
-    console.log('ELEMENTO', ele);
-  }
 }
 
 function pintarListaEnHTML () {
-  listaEnString = contenidoDeLista.join('')
-  miLista.innerHTML = listaEnString;
+  miLista.innerHTML = '';
+
+  contenidoDeLista.forEach((titulo) => {
+    miLista.insertAdjacentHTML('beforeend', `<li class="item"><h5 class="item-name">${titulo}</h5><div class="item-icons"><a href="#" class="complete-item item-icon"><button>Completar</button></a><a href="#" class="edit-item item-icon"><button>Editar</button></a></div></li>` );
+    manejarElemento(titulo);
+  });
 }
 
-function elimarTarea() {
-  console.log('ELIMINAR LA TAREA');
+function manejarElemento(titulo) {
+  let elementosDeLista = miLista.querySelectorAll('.item');
+
+  elementosDeLista.forEach((elemento) => {
+    if(elemento.querySelector('.item-name').textContent === titulo) {
+      //escucha de edicion
+      elemento.querySelector('.edit-item').addEventListener('click', () => {
+        miEntrada.value = titulo;
+        miLista.removeChild(elemento);
+        contenidoDeLista = contenidoDeLista.filter((e) => e !== titulo);
+      });
+      //escucha de completar
+      elemento.querySelector('.complete-item').addEventListener('click', () => {
+        miLista.removeChild(elemento);
+        contenidoDeLista = contenidoDeLista.filter((e) => e !== titulo);
+        localStorage.setItem('ListaDeTareas', JSON.stringify(contenidoDeLista));
+      })
+    }
+  });
 }
 
-
-
-// scope es el alcance que tiene mis variables y funciones
-
-// global scoper
-/*
-  let, const o var que se encuentran hasta el tope del documento
-*/
-// function scope
-/*
-var a = 'adios';
-
-function scopeDeFuncion () {
-  var a = 'hola';
-  
-  console.log(a); //hola
+function agregarTarea() {
+  let titulo = miEntrada.value;
+  contenidoDeLista.push(titulo);
+  localStorage.setItem('ListaDeTareas', JSON.stringify(contenidoDeLista));
+  pintarListaEnHTML();
+  miEntrada.value = '';
 }
-console.log(a); //adios
-*/
-
-// block scope
-/*
-function scopeDeBloke() {
-
-  if (true) {
-    let nivelBloque = 'Estoy dentro'; 
-  }
-
-  // console.log(nivelBloque) //Estoy fuera
-}
-*/
